1a597d6e3d515fd354fd791521823af669082895
George-911 <121451697+George-911@users.noreply.github.com>
2025-07-15 16:26:03 +0000

    Update sketch_jul15a.ino

--- sketch_jul15a/sketch_jul15a.ino
+++ sketch_jul15a/sketch_jul15a.ino
@@ -1,5 +1,5 @@
-const int potPin = 4; // Potentiometer connected to GPIO4
-const int ledPin = 2; // LED connected to GPIO2 (on‑board LED for many dev boards)
+const int potPin = 5; // Potentiometer connected to GPIO5
+const int ledPin = 3; // LED connected to GPIO3 
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -7,10 +7,10 @@ void setup() {
 }
 
 void loop() {
-  int potValue  = analogRead(potPin);              // 0‑4095
-  int brightness = map(potValue, 0, 4095, 0, 1023); // 0‑1023
+  int potValue  = analogRead(potPin);              
+  int brightness = map(potValue, 0, 4095, 0, 1023); 
 
-  for (int i = 0; i < 50; i++) {                   // crude PWM
+  for (int i = 0; i < 50; i++) {                   
     digitalWrite(ledPin, HIGH);
     delayMicroseconds(brightness);
     digitalWrite(ledPin, LOW);
