f5cc868673a88d5be6eacf75a2e225f178fc84b9
George-911 <121451697+George-911@users.noreply.github.com>
2025-07-26 16:06:40 +0000

    Add files via upload

---
+++ printing basic strings.py
@@ -0,0 +1,17 @@
+print("Kofi found an old pen under his bed. \nCurious, he used it to write in his notebook." \
+"\nTo his surprise, every word he wrote came true. \nHe wrote: Tomorrow, I will find a bag of gold.The next morning," \
+" he tripped over something at his doorstep — a small, dusty bag filled with golden coins.\n" \
+"He smiled… and picked up the pen again.")
+
+print("she said,'python is awesome'")
+
+message = "He said, \" don't worry about it.\""
+print(message)
+print("she said, 'today is a beautiful day'")
+print("shopping list: \n\t 1. Bread \n\t 2. eggs \n\t 3. milk \n\t 4. bananas")
+name = "george"
+age = 18
+height= 183
+is_student = "true"
+ 
+print(f"My name is:{name}. I am {age} years old. My height is: {height}, and it's {is_student} that am a student")
