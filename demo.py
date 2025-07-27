537e83d2a8e7c542322f9af09ddebd095ba6d338
George-911 <121451697+George-911@users.noreply.github.com>
2025-07-27 01:51:26 +0000

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,17 @@
+item_cost = 12.56
+quantity = 3
+total_cost = item_cost * quantity
+
+print(f"total cost: $ {total_cost}")
+
+
+addition = 5 + 4
+subtraction = 12 - 4
+multiplication = 12 * 7
+
+exponential = 2 ** 5
+floor_division = 17 // 4
+modulus = 15 % 4
+combine = (5 * 5) + 6
+ 
+print(f"all the values; {addition,subtraction, multiplication, exponential, floor_division, modulus,combine}")
