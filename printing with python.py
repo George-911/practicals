cc9cbcd084173c0ba98b9ab76009bf6a7f180a3b
George-911 <121451697+George-911@users.noreply.github.com>
2025-07-27 01:52:47 +0000

    Add files via upload

---
+++ printing with python.py
@@ -0,0 +1,15 @@
+print("hello world")
+
+print("I'm learning Python")
+print("this is so much fun!")
+print("I can't wait to learn more!")
+
+print("this is a string with  double quotes.")
+print('this is a string with a single quotes')
+
+print("he said 'python is awesome'")
+print('she said "i totally agree"')
+
+print("first line \nsecond line")
+print("this \tis \tawesome")
+print("my name is", "george", "and i am", 30, "years old")
