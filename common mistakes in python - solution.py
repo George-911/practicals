80a0242f9e505c601dda70bc4bf7e6e5f4f90d2f
George-911 <121451697+George-911@users.noreply.github.com>
2025-07-27 01:52:15 +0000

    Add files via upload

---
+++ common mistakes in python - solution.py
@@ -0,0 +1,6 @@
+print('let\'s practice finding errors')
+print('let\'s practice finding errors')
+print('what\'s wrong with this line?')
+print('fix me')    
+print('what is missing here?')
+print("what do i use" "incorrectly here?")          
