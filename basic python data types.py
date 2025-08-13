1dd380a324a90618cb0d25e50d52c05b5e1f2bbd
George-911 <121451697+George-911@users.noreply.github.com>
2025-08-13 02:15:08 +0000

    Add files via upload

---
+++ basic python data types.py
@@ -0,0 +1,11 @@
+#personal information
+name = "george"  #str
+age = 18  # int
+height_meters = 1.83 #float
+is_student = True #bool
+
+#print out the information
+print("Name", name)
+print("Age", age)
+print("Hieght", height_meters)
+print("Are you a student", is_student)
