8cefb537624c4b5fda43ad57fad802bf83d81986
George-911 <121451697+George-911@users.noreply.github.com>
2025-09-10 22:18:53 +0000

    Add files via upload

---
+++ addition operation with numbers and string concatenation.py
@@ -0,0 +1,28 @@
+print(" i like this food " +  " and i don't like that food")
+print(3 + 4 + 3)
+print( 3 + 0.5 )
+print("this notebook belongs to " + "george")
+sm_number = 5
+big_number = 6
+print(sm_number + big_number)
+first_name = "george "
+print(first_name + "remember to save the notebook frequently")
+print("My name", "is", "george")
+print("I am",8 , "years older than my niece",10 , "who is years old")
+
+house_number = 23
+street_name = "edouado modhlana"
+ci_ty = "Greater Accra"
+print("my house number is", house_number , "and the street name is", street_name , "in the city of", ci_ty)
+age = int(input("enter your age:"))
+name = input("what is your name")
+print("my name is ", name, "and i am ",age,"years old")
+
+party_name = input("enter the party name;")
+attendees = int(input("How many people are attending? "))
+time = input("How many days will the training session last")
+print("congrats your reservation has been reviewed successfully with ",party_name, "with", attendees ," people ", "for",time,"days")
+
+print("she's a very good person.")
+print('he told me "shut up" and i vexed ')
+print("""she said, "i don't like how you dress" """)
