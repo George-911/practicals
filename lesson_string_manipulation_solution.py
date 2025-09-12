eeafd9d44e5de22d6da175d8e100c2d1c6ce026f
George-911 <121451697+George-911@users.noreply.github.com>
2025-09-12 02:00:53 +0000

    Add files via upload

---
+++ lesson_string_manipulation_solution.py
@@ -0,0 +1,20 @@
+# create a variable first_name and assign to it your first name.
+first_name = "George"
+
+# create a variable last_name and assign to it your last name.
+last_name = "Amoah"
+
+# create a variable full_name by concatenating your first_name and last_name with a space in between.
+full_name = first_name + " " + last_name
+
+# Print out the full_name.
+print(f"Full name : {full_name}")
+
+# Print out the length of full_name using the len() function.
+print(f"length of full name: {len(full_name)}")
+
+# Print out the full_name in all uppercase letters.
+print(f"full name in upper case: {full_name.upper()}")
+
+# Print out the full_name in all lowercase letters.
+print(f"full name in lower case: {full_name.lower()}")
