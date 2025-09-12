ba3fe59d0de5898acc277c0abebfb267c35e1b79
George-911 <121451697+George-911@users.noreply.github.com>
2025-09-12 02:01:49 +0000

    Add files via upload

---
+++ lesson_type_conversion_solution.py
@@ -0,0 +1,21 @@
+# Create a variable called 'price' as a string with the value '19.99'.
+price = "19.99"
+
+# Convert 'price' to a float and store it in a new variable called 'price_float'.
+price_float = float(price)
+
+# Create a variable called 'quantity' as an integer with a value of 5.
+quantity = 5
+
+# Calculate the total cost by multiplying 'price_float' and 'quantity' and store it in a variable called total_cost.
+total_cost = price_float * quantity
+
+# Covert the total_cost to an integer {round down} and store it in a variable called 'total_cost_int'
+total_cost_int = int(total_cost)
+
+# Print out 'price', 'price_float', 'quantity', 'total_cost' and 'total_cost_int'Print(f"price (string):{price} , Type; {type(price)} ")
+print(f"price (string):{price} , Type; {type(price)} ")
+print(f"price (float):{price_float} , Type; {type(price_float)} ")
+print(f"Quantity:{quantity} , Type; {type(quantity)} ")
+print(f"total cost :{total_cost} , Type; {type(total_cost)} ")
+print(f"total cost (int):{total_cost_int} , Type; {type(total_cost_int)} ")
