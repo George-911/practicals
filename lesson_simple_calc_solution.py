5b4e714b4b2336b67cb1a161bfeb15dcb1eadb8a
George-911 <121451697+George-911@users.noreply.github.com>
2025-09-12 02:00:01 +0000

    Add files via upload

---
+++ lesson_simple_calc_solution.py
@@ -0,0 +1,17 @@
+# 1. Create two variables num1 and num2, and assign them values.
+num1 = 10
+num2 = 3
+
+# 2. Perform the following operations and print the results.
+# Addition of num1 and num2
+print(f"addition of: {num1 + num2}")
+# Substraction of num1 and num2
+print(f"substraction of: {num1 - num2}")
+# Multiplication of num1 and num2
+print(f"multiplication of: {num1 * num2}")
+# Division of num1 and num2
+print(f"division of: {num1 / num2}")
+# Integer devision of num1 and num2
+print(f"integer division of: {num1 // num2}")
+# Remainder of num1 and dvided by num2
+print(f"Remainder: {num1 % num2}")
