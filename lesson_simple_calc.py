fda0b4917e3da208a95c1c305296802aae86ad96
George-911 <121451697+George-911@users.noreply.github.com>
2025-09-12 01:59:34 +0000

    Add files via upload

---
+++ lesson_simple_calc.py
@@ -0,0 +1,9 @@
+# 1. Create two variables num1 and num2, and assign them values.
+
+# 2. Perform the following operations and print the results.
+# Addition of num1 and num2
+# Substraction of num1 and num2
+# Multiplication of num1 and num2
+# Division of num1 and num2
+# Integer devision of num1 and num2
+# Remainder of num1 and dvided by num2
