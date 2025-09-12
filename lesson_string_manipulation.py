4b89bc4ca7af13cf02fd63ae8a64767b26338b1d
George-911 <121451697+George-911@users.noreply.github.com>
2025-09-12 02:00:31 +0000

    Add files via upload

---
+++ lesson_string_manipulation.py
@@ -0,0 +1,13 @@
+# create a variable first_name and assign to it your first name.
+
+# create a variable lastt_name and assign to it your last name.
+
+# create a variable full_name by concatenating your first_name and last_name with a space in between.
+
+# Print out the full_name.
+
+# Print out the length of full_name using the len() function.
+
+# Print out the full_name in all uppercase letters.
+
+# Print out the full_name in all lowercase letters.
